7e7f1e0b79bbcb507a4c8b93abdb6b70396e3516
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-06 23:05:32 -0500

    connected to Django api

--- app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
+++ app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
@@ -5,16 +5,18 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 data class Todo(
-    var userId: Int,
+//    var userId: Int,
     var id: Int,
     var title: String,
     var completed: Boolean
 )
 
-const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+//const val BASE_URL = "https://jsonplaceholder.typicode.com/"
+const val BASE_URL = "http://10.0.2.2:8080/api/"
+// should be run on: python3 manage.py runserver 8080
 
 interface ApiService{
-    @GET("todos")
+    @GET("task-list")
     suspend fun getTodos(): List<Todo>
 
     companion object{
