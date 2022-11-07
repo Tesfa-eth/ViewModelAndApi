dd2e023c8b297984d3686bebc9b0a4201aabad9f
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-07 01:56:33 -0500

    commit

--- app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
+++ app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
@@ -18,7 +18,8 @@ data class Todo(
 )
 
 //const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-const val BASE_URL = "http://127.0.0.1:8000/api/"
+//const val BASE_URL = "http://127.0.0.1:8000/api/"
+const val BASE_URL = "http://10.0.2.2:8080/api/"
 
 interface ApiService{
     @GET("onsale-items")
