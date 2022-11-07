ed0662a85846d030394061632d9e1f7ae6bd03fa
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-07 03:13:08 -0500

    working api call
    Need to rename data class

--- app/src/main/java/com/example/viewmodelsandapitutorial/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelsandapitutorial/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.viewmodelsandapitutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -10,7 +12,13 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
@@ -49,7 +57,7 @@ fun TodoView(vm: TodoViewModel) {
         },
         content = {
             if (vm.errorMessage.isEmpty()){
-                Column(modifier = Modifier.padding(16.dp)) {
+                Column(modifier = Modifier.padding(0.dp)) {
                     LazyColumn(modifier = Modifier.fillMaxHeight()){
                         items(vm.todoList){todo ->
                             Column{
@@ -59,20 +67,14 @@ fun TodoView(vm: TodoViewModel) {
                                         .padding(16.dp),
                                     horizontalArrangement = Arrangement.SpaceBetween
                                 ){
-                                    Box(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(0.dp, 0.dp, 16.dp, 0.dp)
-                                    ){
-                                        Text(text = todo.name, maxLines = 1, overflow = TextOverflow.Ellipsis)
-                                    }
-                                    
-                                    Spacer(modifier = Modifier.width(16.dp))
-                                    
-                                    Checkbox(checked = todo.sold, onCheckedChange = null)
+                                    CardView(
+                                        itemName = todo.name, 
+                                        price = todo.price, 
+                                        sold = todo.sold, 
+                                        seller = todo.seller, 
+                                        negotiable = todo.negotiable, 
+                                        condition=todo.condition)
                                 }
-                                
-                                Divider()
                             }
                         }
                     }
@@ -83,6 +85,86 @@ fun TodoView(vm: TodoViewModel) {
             }
         }
     )
+}
+
+@Composable
+fun CardView(itemName: String,
+             price: Float,
+             sold: Boolean,
+             seller: String,
+             negotiable: Boolean,
+             condition: String,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .clickable { },
+        elevation = 10.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(15.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.on_sale_image),
+                contentDescription = "On sell item",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(20.dp)
+            )
+            Text(
+                buildAnnotatedString {
+                    //append("Item name: ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B8))
+                    ) {
+                        append(itemName)
+                    }
+                }
+            )
+            Text(
+                buildAnnotatedString {
+
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900)) {
+                        append(price.toString())
+                    }
+                    append(" USD")
+                }
+            )
+
+            Text(
+                buildAnnotatedString {
+                    //append("Seller: ")
+                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900, color = Color(0xFF4552B8))
+                    ) {
+                        append(seller)
+                    }
+                }
+            )
+
+            if (negotiable){
+                //append("Negotiable")
+                //Text(text = "Negotiable", color = Color.Green)
+                Text(
+                    buildAnnotatedString {
+                        //append("Seller: ")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.DarkGray)
+                        ) {
+                            append("Negotiable")
+                        }
+                    }
+                )
+            }
+            else {
+                Text(
+                    buildAnnotatedString {
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.SemiBold, color = Color.LightGray)
+                        ) {
+                            append("Non-negotiable")
+                        }
+                    }
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
