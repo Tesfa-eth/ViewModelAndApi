cec21837d3670d97776d1654a0f9ffd44a069f9e
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-11-18 20:48:32 -0500

    commit

--- app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
+++ app/src/main/java/com/example/viewmodelsandapitutorial/api/ApiService.kt
@@ -19,7 +19,7 @@ data class Todo(
 
 //const val BASE_URL = "https://jsonplaceholder.typicode.com/"
 //const val BASE_URL = "http://127.0.0.1:8000/api/"
-const val BASE_URL = "http://10.0.2.2:8080/api/"
+const val BASE_URL = "https://btonmarketapi.herokuapp.com/api/"
 
 interface ApiService{
     @GET("onsale-items")
